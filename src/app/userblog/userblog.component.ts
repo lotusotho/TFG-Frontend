3bbb4ffa7e2546ce88dd4a7b355c8125602d5504
Alejandro Ramos <lotusotho11@gmail.com>
2025-02-15 23:43:55 +0100

    Add loading animation

--- src/app/userblog/userblog.component.ts
+++ src/app/userblog/userblog.component.ts
@@ -2,18 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ContentService } from '../services/content.service';
 import { MarkdownModule } from 'ngx-markdown';
-import { DatePipe } from '@angular/common';
+import { DatePipe, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-userblog',
   standalone: true,
-  imports: [MarkdownModule, DatePipe],
+  imports: [MarkdownModule, DatePipe, NgIf],
   templateUrl: './userblog.component.html',
   styleUrl: './userblog.component.css',
 })
 export class UserblogComponent implements OnInit {
   userContent: any = '';
   username: string = '';
+  isLoading: boolean = true;
 
   constructor(
     private contentService: ContentService,
@@ -27,11 +28,15 @@ export class UserblogComponent implements OnInit {
         this.contentService.getUserContentQuery(blog).subscribe(
           (response: any) => {
             this.userContent = response.data[0];
+            this.isLoading = false;
           },
           (error) => {
             console.error('Error fetching user content:', error);
+            this.isLoading = false;
           }
         );
+      } else {
+        this.isLoading = false;
       }
     });
   }
