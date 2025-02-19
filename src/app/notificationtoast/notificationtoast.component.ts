b473a1bacf2a7f16f47e28a5971c51e8d8af1783
Alejandro Ramos <lotusotho11@gmail.com>
2025-02-19 13:03:07 +0100

    Change notification

--- src/app/notificationtoast/notificationtoast.component.ts
+++ src/app/notificationtoast/notificationtoast.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, Inject, Input, AfterViewInit } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { PLATFORM_ID } from '@angular/core';
 
 @Component({
   selector: 'app-notificationtoast',
@@ -8,6 +9,31 @@ import { CommonModule } from '@angular/common';
   templateUrl: './notificationtoast.component.html',
   styleUrls: ['./notificationtoast.component.css'],
 })
-export class NotificationtoastComponent {
+export class NotificationtoastComponent implements AfterViewInit {
   @Input() notificationType: string = '';
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      const deleteButtons = document.querySelectorAll('.notification .delete');
+      deleteButtons.forEach((deleteButton) => {
+        const notificationEl = deleteButton.parentNode;
+        deleteButton.addEventListener('click', () => {
+          if (notificationEl && notificationEl.parentNode) {
+            notificationEl.parentNode.removeChild(notificationEl);
+          }
+        });
+      });
+
+      const notifications = document.querySelectorAll('.notification');
+      notifications.forEach((notification) => {
+        setTimeout(() => {
+          if (notification.parentNode) {
+            notification.parentNode.removeChild(notification);
+          }
+        }, 3000);
+      });
+    }
+  }
 }
