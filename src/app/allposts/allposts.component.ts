87025202427297fd5ce6735b64fead0c34e321c3
Alejandro Ramos <lotusotho11@gmail.com>
2025-02-19 23:06:50 +0100

    Buscar posts

--- src/app/allposts/allposts.component.ts
+++ src/app/allposts/allposts.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { ContentService } from '../services/content.service.js';
 import { DatePipe, NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-allposts',
   standalone: true,
-  imports: [NgFor, DatePipe],
+  imports: [NgFor, DatePipe, FormsModule],
   templateUrl: './allposts.component.html',
   styleUrl: './allposts.component.css',
 })
 export class AllpostsComponent implements OnInit {
-  allPostsData: any = '';
+  allPostsData: any[] = [];
+  filteredPosts: any[] = [];
+  searchQuery: string = '';
+
   constructor(private contentService: ContentService) {}
 
   ngOnInit(): void {
@@ -25,10 +29,23 @@ export class AllpostsComponent implements OnInit {
             new Date(b.date_updated).getTime() -
             new Date(a.date_updated).getTime()
         );
+        this.filteredPosts = this.allPostsData;
       },
       error: (error: any) => {
         console.log('Error getting all posts', error);
       },
     });
+  }
+
+  filterPosts(): void {
+    if (this.searchQuery.trim() === '') {
+      this.filteredPosts = this.allPostsData;
+    } else {
+      this.filteredPosts = this.allPostsData.filter((post: any) =>
+        post.user.username
+          .toLowerCase()
+          .includes(this.searchQuery.toLowerCase())
+      );
+    }
   }
 }
