60b7f63ee205c4cff6c6e2137486ce388eedb65c
Alejandro Ramos <lotusotho11@gmail.com>
2025-06-05 20:09:14 +0200

    feat: add more modal stuff

--- src/app/featuredpostmodal/featuredpostmodal.component.ts
+++ src/app/featuredpostmodal/featuredpostmodal.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { NgFor } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-featuredpostmodal',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, RouterLink],
   templateUrl: './featuredpostmodal.component.html',
   styleUrl: './featuredpostmodal.component.css',
 })
